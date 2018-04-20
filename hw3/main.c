ddae86ba4100eef5c3fa0a1287f0bda11b6c9b50
Ryan Edelstein <redelste@stevens.edu>
2018-04-20 14:48:52 -0400

    Added timestamp function, and function to create new Directory node: DIR_NODE, fixed parseFilesList

--- hw3/main.c
+++ hw3/main.c
@@ -43,10 +43,6 @@ typedef struct lfile{
   struct lfile *next;
 } lfile;
 
-// typedef struct fileHelp{
-//   nodeType type;
-//   char *name;
-// } node;
 /*
   Each node in the directory tree G is associated with either a regular file or
   a directory.
@@ -59,6 +55,46 @@ typedef enum{
 } nodeType; //node in the directory
 
 
+
+/*
+  A tree node represented by the data provided by parsing the files.
+*/
+typedef struct node{
+  nodeType nt;
+  char *file; //file names --  root
+  struct node *left;
+  struct node *right;
+} node;
+/*
+  Returns the current time_stamp
+*/
+char *timeStamp(){
+char *timestamp = (char *)malloc(sizeof(char) * 16);
+time_t ltime;
+ltime=time(NULL);
+struct tm *tm;
+tm=localtime(&ltime);
+sprintf(timestamp,"%04d%02d%02d%02d%02d%02d", tm->tm_year+1900, tm->tm_mon,
+    tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
+return timestamp;
+}
+
+
+/*
+Each node in G that corresponds to a regular file, stores the attributes (name,
+size, and time-stamp) of the file and a pointer to Lfile, the linked-list data
+structure storing the disk block addresses of the file. An example of the
+directory tree (and other two data structures) are shown below.
+*/
+// A utility function to create a new tree node for the directory node type.
+struct node* newNode(char *file, node *parent)
+{
+    struct node* temp = (struct node*) malloc(sizeof(struct node));
+    temp->type = DIR_NODE;
+    temp->name = name;
+    temp->parent = parent;
+    return temp;
+}
 /*
   parse the file_list.txt file.
   The input is the file_list.txt, root is the root node representing the root
@@ -73,18 +109,20 @@ typedef enum{
 void parseFileList(FILE* fl, node *root, ldisk *disk, int blockSize){
   int status1;
   unsigned long size; //file size;
+  char month[4] = {0}; //integer array
+  char dateAndTime[10];
   if(fl == NULL){
     printf("Cannot open file!\n");
   }
   else{
       do{
-        status1 = fscanf(fl, "%s");
+        status1 = fscanf(file,  " %*d %*d %*s %*d %*s %*s %lu %s %d %s %[^\n]\n", &size, month, &day, dateAndTime,fileName);
+        printf("%d %s %d %d:%d",month, day, dateAndTime);
     }while(status1 != -1);
   }
   fclose(fl);
-  
-}
 
+}
 int main(int argc, char *argv[]){
   /*
     Your program must accept the following parameters at the command prompt:
